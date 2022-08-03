71f1ba17567b91ba389cc4ea77197b22857b222f
gabl <78044893+gabl22@users.noreply.github.com>
2022-08-03 11:58:36 +0200

    Lottery now uses TimeRandom

--- Lottery.sol
+++ Lottery.sol
@@ -8,10 +8,16 @@ import "./Ownable.sol";
 
 contract Lottery is Ownable {
 
+    using TimeRandom for TimeRandom.Random;
+
+    TimeRandom.Random private generator = TimeRandom.Random({
+        _last: 2*3*5*7*11*13*17*19*23*29*31*37*41*43*47*53*59*61*67*71 + 1
+    });
+
     function betOn(uint min, uint bet, uint max) external payable returns(bool) {
         require(min <= bet && bet <= max, "Number out of range");
         require(min != max, "Range can't be empty");
-        if(TimeRandom.randomInt(min, max) == bet) {
+        if(TimeRandom.random(generator.nextSeed(), min, max) == bet) {
             uint prize = (max - (min + 1)) * msg.value;
             address winner = tx.origin;
             if(balance() < prize) {
@@ -22,7 +28,7 @@ contract Lottery is Ownable {
         }
         return false;
     }
-
+    
     function balance() public view returns(uint) {
         return address(this).balance;
     }
