4d3cfc279a4c3c44571a3414fb6f53658a0f9064
gabl <gabl22ytmc@gmail.com>
2022-08-03 20:22:49 +0200

    Fair Lottery

--- Lottery.sol
+++ Lottery.sol
@@ -1,14 +1,21 @@
 // SPDX-License-Identifier: MIT
 // gabl22 @ github.com
 
-// Lottery 0x01 03.08.2022
+// Lottery 0x02 03.08.2022
 
 pragma solidity >=0.8.0 <0.9.0;
 
 import "./TimeRandom.sol";
-import "./Ownable.sol";
+import "./CashFlow.sol";
 
-contract Lottery is Ownable {
+contract Lottery is CashFlow
+(
+    CashFlow.Config({
+        publicDonations: true,
+        publicCharging: true
+    })
+)
+{
 
     using TimeRandom for TimeRandom.Random;
 
@@ -16,15 +23,13 @@ contract Lottery is Ownable {
         _last: 557940830126698960967415390
     });
 
-   function betOn(uint min, uint bet, uint max) external payable returns(uint) {
+   function betOn(uint min, uint bet, uint max) external payable cashFlow returns(uint) {
         require(min <= bet && bet <= max, "Error: Number out of range");
         require(min != max, "Error: Range can't be empty");
+        uint _payout = prize(msg.value, min, max);
+        require(_payout < balance(), "Error: Insufficient funds, consider charging!");
         if(TimeRandom.random(generator.nextSeed(), min, max) == bet) {
-            uint _payout = prize(msg.value, min, max);
             address winner = tx.origin;
-            if(balance() < _payout) {
-                _payout = balance();
-            }
             payable(winner).transfer(_payout);
             return _payout;
         }
