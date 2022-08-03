eb2d11c0951aeffe64f5a363fda98dc717da0f39
gabl <78044893+gabl22@users.noreply.github.com>
2022-08-03 11:57:29 +0200

    TimeRandom now a library

--- TimeRandom.sol
+++ TimeRandom.sol
@@ -5,15 +5,32 @@ pragma solidity >=0.8.0 <0.9.0;
 
 library TimeRandom {
 
-    function randomInt(uint min, uint max) external view returns(uint) {
-        return randomInt(max - min) + min;
+    struct Random {
+        uint _last;
     }
 
-    function randomInt(uint max) public view returns(uint) {
-        return time() % max;
+    function nextSeed(Random storage rnd) internal returns(uint) {
+        rnd._last = nextViewSeed(rnd);
+        return lastSeed(rnd);
     }
 
-    function time() public view returns(uint) {
-        return block.timestamp;
+    function nextViewSeed(Random storage rnd) internal view returns(uint) {
+        return uint(keccak256(abi.encodePacked(lastSeed(rnd) + block.timestamp + block.number)));
+    }
+    
+    function nextPureSeed() internal view returns(uint) {
+        return uint(keccak256(abi.encodePacked(block.timestamp + block.number)));
+    }
+
+    function lastSeed(Random storage rnd) internal view returns(uint) {
+        return rnd._last;
+    }
+
+    function random(uint seed, uint max) internal pure returns(uint) {
+        return seed % max;
+    }
+
+    function random(uint seed, uint min, uint max) internal pure returns(uint) {
+        return random(seed, min - max) + min;
     }
 }
