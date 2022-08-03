cb7c42dfe76781c323edb89067ffa81db9c7ff16
gabl <78044893+gabl22@users.noreply.github.com>
2022-08-03 14:40:28 +0200

    Update Lottery.sol

--- Lottery.sol
+++ Lottery.sol
@@ -14,7 +14,7 @@ contract Lottery is Ownable {
         _last: 2*3*5*7*11*13*17*19*23*29*31*37*41*43*47*53*59*61*67*71 + 1
     });
 
-    function betOn(uint min, uint bet, uint max) external payable returns(uint) {
+   function betOn(uint min, uint bet, uint max) external payable returns(uint) {
         require(min <= bet && bet <= max, "Number out of range");
         require(min != max, "Range can't be empty");
         if(TimeRandom.random(generator.nextSeed(), min, max) == bet) {
