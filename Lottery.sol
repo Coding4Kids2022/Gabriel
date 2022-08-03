fface023ffd38004ec78ebdf3fe76445421983c5
gabl <78044893+gabl22@users.noreply.github.com>
2022-08-03 13:55:47 +0200

    Now returning integer

--- Lottery.sol
+++ Lottery.sol
@@ -14,7 +14,7 @@ contract Lottery is Ownable {
         _last: 2*3*5*7*11*13*17*19*23*29*31*37*41*43*47*53*59*61*67*71 + 1
     });
 
-    function betOn(uint min, uint bet, uint max) external payable returns(bool) {
+    function betOn(uint min, uint bet, uint max) external payable returns(uint) {
         require(min <= bet && bet <= max, "Number out of range");
         require(min != max, "Range can't be empty");
         if(TimeRandom.random(generator.nextSeed(), min, max) == bet) {
@@ -24,9 +24,9 @@ contract Lottery is Ownable {
                 prize = balance();
             }
             payable(winner).transfer(prize);
-            return true;
+            return prize;
         }
-        return false;
+        return 0;
     }
     
     function balance() public view returns(uint) {
